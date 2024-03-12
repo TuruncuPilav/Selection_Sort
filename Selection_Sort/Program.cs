96296498481fac7b78aff1f3015820d2b4ae4df1
Berkay Özdemir <berkayozdemir960@hotmail.com>
2024-03-12 10:44:07 +0300

    Selection_Sort

---
+++ Selection_Sort/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[20]; //Dizi nesnesini oluşturur.
+            Random rnd = new Random(); //Random nesnesini oluşturur.
+
+            for (int i = 0; i < numbers.Length; i++) //Dizi nesnesinin uzunluğu kadar döngüye girer.
+                numbers[i] = rnd.Next(1, 100); //İndislere rastgele değerler atar.
+
+            int temp, small;
+
+            for (int i = 0; i < numbers.Length; i++) //Üst döngü, Sırası ile tüm indisleri dolaşır.
+            {
+                small = i;
+                for (int j = i; j < numbers.Length; j++) //Alt döngü, üst döngüde dönen elemanı tüm indislerle karşılaştırır.
+                {
+                    if (numbers[small] >= numbers[j])
+                    {
+                        small = j;
+                    }
+                }
+
+                temp = numbers[i];
+                numbers[i] = numbers[small];
+                numbers[small] = temp;
+            }
+
+            foreach (var item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.Read();
+        }
+    }
+}
